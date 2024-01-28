9778a9b289e8558970b69e3a2ca3d0f709d31b21
Felipe Melo <melolemoslima@gmail.com>
2024-01-27 22:59:43 -0300

    correção

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const form = document.getElementById('container');
-const campoA = document.getElementById('campoA');
-const campoB = document.getElementById('campoB');
+const form = document.getElementById('form');
+const campoA = document.getElementById('saldo');
+const campoB = document.getElementById('saque');
 const valido = document.getElementById('valido');
 const erro = document.getElementById('erro');
 
-function validaValor(campoB, campoA) {
-    return campoB > campoA;
+function validaValor(valorA, valorB) {
+    return valorB > valorA;
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    let isValid = validaValor(campoA.value, campoB.value);
+    let isValid = validaValor(parseFloat(campoA.value), parseFloat(campoB.value));
     if (isValid) {
-        valido.textContent = 'Valores válidos!';
+        valido.textContent = 'Valores validos!';
         valido.style.display = 'block';
-        erro.style.display = 'block';
+        erro.style.display = 'none';
         campoA.value = '';
         campoB.value = '';
     } else {
         valido.style.display = 'none';
-        erro.textContent = 'O valor é inválido!';
+        erro.textContent = 'O valor não válido!';
         erro.style.display = 'block';
     }
 });
